30a54f2eef5c605d56d99b3149a099a7ca337153
GK-Leyton <heiderleyton22@gmail.com>
2025-01-28 18:21:24 -0500

    initial commit

---
+++ app/src/main/java/com/heiderleyton/horoscapp/ui/home/MainActivity.kt
@@ -0,0 +1,43 @@
+ package com.heiderleyton.horoscapp.ui.home
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.heiderleyton.horoscapp.R
+import com.heiderleyton.horoscapp.databinding.ActivityMainBinding
+
+ class MainActivity : AppCompatActivity() {
+
+     private lateinit var binding: ActivityMainBinding
+     private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        initUi()
+    }
+
+     private fun initUi() {
+         initNavigation()
+     }
+
+     private fun initNavigation() {
+         val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+         navController = navHost.navController
+         binding.bottomNavView.setupWithNavController(navController)
+     }
+ }
